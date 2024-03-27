220022eacbb1d56e7dd18b02b2c7839752be8c96
tal.gadasi <tal.gadasi@TAL-GADASI-LP>
2024-03-27 14:42:01 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_REAL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            aaa.Visibility= Visibility.Visible;
+            Check_commands_lable.Visibility = Visibility.Hidden;
+            MCU_Programing.Visibility = Visibility.Hidden;
+            Realys_board_command_generate.Visibility = Visibility.Hidden;
+            UART_Communication.Visibility = Visibility.Hidden;
+            Add_test_equipment_and_commands.Visibility = Visibility.Hidden; 
+            Path.Visibility = Visibility.Hidden;
+        }
+
+        private void button4_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            aaa.Visibility = Visibility.Visible;
+            Path.Visibility = Visibility.Visible;
+            Check_commands_lable.Visibility= Visibility.Visible;
+            MCU_Programing.Visibility= Visibility.Visible ;
+            Realys_board_command_generate.Visibility= Visibility.Visible ;
+            UART_Communication.Visibility= Visibility.Visible ;
+            Add_test_equipment_and_commands.Visibility = Visibility.Visible;
+        }
+
+        
+    }
+}
